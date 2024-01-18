541057c6a90c50e6f3b5880a4422c5b49f436551
emrmason <mas20021@byui.edu>
2024-01-18 10:48:03 -0800

    working on MongoDB connection

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 
-
 app.use('/', require('./routes'));
 
 // MOVED TO ..routes/index.js
--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,22 @@ const name2 = (req, res, next) => {
     res.send("The boy who lived");
 };
 
-module.exports = { name1, name2 };
+const { MongoClient } = require("mongodb");
+// const username = encodeURIComponent("emasoncse341");
+// const password = encodeURIComponent("thisismylongpassword");
+
+async function main() {
+    const uri = process.env.DATABASE_URL;
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+
+};
+
+module.exports = { name1, name2, main };
