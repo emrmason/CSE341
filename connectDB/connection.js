b7736cbff348346e5178cd2cc41e1d75b7d229a1
emrmason <mas20021@byui.edu>
2024-01-20 15:10:28 -0800

    renamed server to app.js

--- connectDB/connection.js
+++ connectDB/connection.js
@@ -6,35 +6,29 @@ const connectDB = async () => {
     const client = new MongoClient(URI);
     try {
         await client.connect(); 
-        // await listDatabases(client);
-        await listContacts(client);
-        await singleContact(client);
     } catch (error) {
         console.log(error);
-    } // finally {
-    //     await client.close();
-    // }    
+    }   
+    return client;
 };
 
-// async function listDatabases(client){
-//     const databaseslist = await client.db().admin().listDatabases();
-//     console.log("databases:");
-//     databaseslist.databases.forEach(db => console.log(`-${db.name}`));
-// }
 
 async function listContacts(client) {
     const contactList = await client.db("Test").collection("Contacts").find().forEach(function(myDoc){
-        console.log("Name:" + myDoc.firstName + " " + myDoc.lastName );
+        const contactList = [];
+        contactList.push(myDoc);
+        console.log(contactList);
     });
     // const results = await contactList.toArray();
     // console.log(contactList);
 }
 
 async function singleContact(client) {
-    const oneContact = await client.db("Test").collection("Contacts").find().forEach(function(myDoc){
+    const contacts = await client.db("Test").collection("Contacts").find().forEach(function(myDoc){
         const contactArray = []
         contactArray.push(myDoc._id);
-        console.log(contactArray);
+        //console.log(contactArray);
     })
+    const oneContact = 
 }
 module.exports = connectDB;
