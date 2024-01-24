aed937ec8276bcd51296498fa370fc60da975845
emrmason <mas20021@byui.edu>
2024-01-23 17:31:05 -0800

    LOCALHOST WORKS!!

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -20,13 +20,13 @@ const listContacts = async(req, res, next) => {
 
 const singleContact = ('/:contacts', async(req, res, next) => {
     try{
-        const userID = new ObjectID(req.params._id);
+        const userID = new ObjectID(req.params.id);
         const client = await mongodb.connectDB();
         const collection = client.db("Test").collection("Contacts");
-        const result = await collection.find({_id: userID}).toArray();
-        if(result){
-            console.log(result[0]);
-            res.send(result[0]);
+        const result = await collection.find({_id : userID}).toArray();
+        if(result.length > 0){
+            // console.log(result);
+            res.send(result);
         } else {
             res.send("Contact not found");
         }
