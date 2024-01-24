fc62c17ce56866d1c360a791fa6d2b4b12512fdc
emrmason <mas20021@byui.edu>
2024-01-24 12:17:49 -0800

    Trying to get back to where render worked.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const controller = require('../controllers');
 
+router.use('/', controller.name1);
 
 router.use('/contacts', require('./contacts'));
-
 
 module.exports = router;
 
